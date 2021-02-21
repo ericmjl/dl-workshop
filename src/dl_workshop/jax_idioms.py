7d33d4a3f6a484d9f1407ff4a534bab6863d7079
Eric Ma <ericmajinglong@gmail.com>
2021-02-20 21:48:44 -0500

    Add answers for loopless loops notebook

--- src/dl_workshop/jax_idioms.py
+++ src/dl_workshop/jax_idioms.py
@@ -1,4 +1,33 @@
-from jax import random, lax
+from jax import random, lax, numpy as np, vmap
+
+
+def loopless_loops_ex1(mat1, mat2):
+    return vmap(np.dot)(mat1, mat2)
+
+
+def loopless_loops_ex2(data):
+    """Data is 3D, of shape (n_datasets, n_rows, n_columns)."""
+
+    def inner(dataset):
+        """dataset is 2D, of shape (n_rows, n_columns)."""
+        cp = vmap(np.cumprod)(dataset)
+        s = vmap(np.sum)(cp)
+        return s
+
+    return vmap(inner)(data)
+
+
+from functools import partial
+
+
+def loopless_loops_ex3(node_feats):
+    def concat(n1, n2):
+        return np.concatenate([n1, n2])
+
+    def concatenate(node, node_feats):
+        return vmap(partial(concat, node))(node_feats)
+
+    return vmap(partial(concatenate, node_feats=node_feats))(node_feats)
 
 
 def make_gaussian_random_walk_func(num_steps):
