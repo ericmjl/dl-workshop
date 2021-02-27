cb83319963249e6f1c836bcd589f9277ff1c0ad6
Eric Ma <ericmajinglong@gmail.com>
2021-02-27 17:26:39 -0500

    Restructured randomness notebook

--- src/dl_workshop/jax_idioms.py
+++ src/dl_workshop/jax_idioms.py
@@ -34,7 +34,7 @@ def loopless_loops_ex3(node_feats):
 
 def lax_scan_ex_1(prev_wealth, time, interest_factor):
     new_wealth = prev_wealth * interest_factor
-    return new_wealth, prev_wealth    
+    return new_wealth, prev_wealth
 
 
 def lax_scan_ex_2(num_breaks: int, frac: float) -> np.ndarray:
@@ -42,22 +42,20 @@ def lax_scan_ex_2(num_breaks: int, frac: float) -> np.ndarray:
         stick = stick_length * frac 
         remainder = stick_length - stick
         return remainder, stick
-    
+
     fracs = np.array([frac] * num_breaks)
     final, sticks = lax.scan(step, init=1.0, xs=fracs)
     return sticks
 
 
+def randomness_ex_1(key, num_breaks, concentration: float):
+    def step(stick_length: float, key, concentration: float):
+        fraction = random.beta(key, a=1, b=concentration)
+        stick = stick_length * fraction
+        remainder = stick_length - stick
+        return remainder, stick
 
-def make_gaussian_random_walk_func(num_steps):
-    def gaussian_random_walk(key):
-        keys = random.split(key, num=num_steps)
-        final, result = lax.scan(generate_new_gaussian, 0.0, keys)
-        return final, result
-
-    return gaussian_random_walk
-
-
-def generate_new_gaussian(old_gaussian, key):
-    new_gaussian = random.normal(key) + old_gaussian
-    return new_gaussian, old_gaussian
+    step_one_conc = partial(step, concentration=concentration)
+    keys = random.split(key, num_breaks)
+    final, sticks = lax.scan(step_one_conc, init=1.0, xs=keys)
+    return final, sticks
